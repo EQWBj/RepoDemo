ec4c9fd39052fc6da20b66e7b8273b694dcbd1df
EQWBj <99023417+EQWBj@users.noreply.github.com>
2022-02-09 15:25:43 +0530

    Update Program.cs

--- RepoDemo/Program.cs
+++ RepoDemo/Program.cs
@@ -10,7 +10,7 @@ namespace RepoDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git Repository all");
+            Console.WriteLine("Hello Git Repository all hi");
         }
     }
 }
